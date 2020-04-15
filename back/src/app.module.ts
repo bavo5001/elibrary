0b329ceb100415912ea50ec3bd2bf0005c031677
angular_ts_8080 <vovabarneckyi@gmail.com>
2020-04-16 01:51:57 +0300

    connect db

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -4,12 +4,39 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
-import {ConfigModule} from "@nestjs/config";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
-  imports: [AuthModule,
-    UsersModule,
-    ConfigModule,
+  imports: [
+      AuthModule,
+      UsersModule,
+      ConfigModule.forRoot({
+        isGlobal:true
+    }),
+    //   TypeOrmModule.forRootAsync({
+    //     imports: [ConfigModule],
+    //     inject: [ConfigService],
+    //       useFactory: async (configService: ConfigService) => ({
+    //         type: 'postgres' as 'postgres',
+    //         host: configService.get<string>('POSTGRES_HOST'),
+    //         port: Number(configService.get('PORT')),
+    //         username: configService.get<string>('POSTGRES_USER'),
+    //         password: configService.get<string>('POSTGRES_PASSWORD'),
+    //         database: configService.get<string>('POSTGRES_DATABASE'),
+    //         entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //         synchronize: true,
+    //   })
+    // })
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        entities: [],
+        synchronize: true,
+      })
   ],
   controllers: [AppController],
   providers: [AppService],
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+require('dotenv').config();
+dotenv.config();
+
 import { AppModule } from './app.module';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
